67561576bf65f6512900458126cbe791dfcea2bc
Gabrielly Venturini <134551503+GabbyVenturini@users.noreply.github.com>
2023-07-13 20:46:58 -0300

    Add files via upload

---
+++ to-do-list.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('.input');
+const ulContainer = document.querySelector('.ulContainer');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const inputValue = event.target.querySelector('input').value.trim();
+
+    if (inputValue.length) {
+        ulContainer.innerHTML +=
+            `<li class="li">
+            <span class="linha-li">${inputValue}</span>
+            <i class="bx bxs-trash lixeira"></i>
+        </li>`;
+
+        form.reset();
+    }
+});
+
+ulContainer.addEventListener('click', event => {
+    const clickElement = event.target;
+    if (clickElement.classList.contains('lixeira')) {
+        clickElement.parentElement.remove();
+    }
+});
